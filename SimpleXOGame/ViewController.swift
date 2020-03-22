2be07f95b2fbdab4b0187171e9fdf71f342129a3
AbdoSobhy <AbdoAhmedSobhy@hotmail.com>
2020-03-22 14:48:39 +0200

    first commit

--- SimpleXOGame/ViewController.swift
+++ SimpleXOGame/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 enum Player {
-    case one // X 1
-    case two  // O 2
+    case one //  X -> 1
+    case two  // O -> 2
 }
 
 
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     var gameBoardState = [0, 0, 0,
                           0, 0, 0,
                           0, 0, 0]
+
     let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                                [0, 3, 6], [1, 4, 7], [2, 5, 8],
                                [0, 4, 8], [2, 4, 6]]
@@ -37,35 +38,47 @@ class ViewController: UIViewController {
         checkForWinner()
     }
     
+    @IBAction func resetFunc(_ sender: UIButton) {
+        reset()
+    }
     private func checkForWinner() {
-        // Check for Draw State
         
         for combination in winningCombinations {
-            if gameBoardState[combination[0]] != 0 && gameBoardState[combination[0]] == gameBoardState[combination[1]] && gameBoardState[combination[1]] == gameBoardState[combination[2]] {
+            if gameBoardState[combination[0]] != 0 &&
+                gameBoardState[combination[0]] == gameBoardState[combination[1]] && gameBoardState[combination[1]] == gameBoardState[combination[2]]
+            {
+            if gameBoardState[combination[0]] == 1 {
+            
                 
-                if gameBoardState[combination[0]] == 1 {
-                    // Alert X Winner 🎉
+                    print("X is winner®")
+                print(combination)
+                print(gameBoardState)
+                reset()
+            
                     // in Alert Action Reset the Game
-                } else {
-                    // Alert O Winner 🎉
+                } else if  gameBoardState[combination[0]] == 2{
+                
+                    print("O is winner®")
+                print(combination)
+                print(gameBoardState)
+                reset()
+
+
+            
                     // in Alert Action Reset the Game
-                }
+            } else if gameBoardState[combination[0]] != 1 && gameBoardState[combination[0]] != 2
+            {
+                
+                print("drow")
+                print(gameBoardState)
+                reset()
+
                 
             }
         }
+        }
+
     }
-    
-    private func updateGameBoardState(_ sunder: UIButton) {
-        let currnetPlayerButtonIndex = sunder.tag
-        gameBoardState[currnetPlayerButtonIndex] = sunder.titleLabel?.text == "X" ? 1 : 2
-        
-//        if sunder.titleLabel?.text == "X" {
-//            gameBoardState[currnetPlayerButtonIndex] = 1
-//        } else {
-//            gameBoardState[currnetPlayerButtonIndex] = 2
-//        }
-    }
-    
     private func currentPlayerPlay(_ sunder: UIButton) {
         switch currentPlayer {
         case .one:
@@ -75,19 +88,31 @@ class ViewController: UIViewController {
             sunder.setTitle("O", for: .normal)
             currentPlayer = .one
         }
-        
         sunder.isEnabled = false
     }
     
+    private func updateGameBoardState(_ sunder: UIButton) {
+        let currnetPlayerButtonIndex = sunder.tag
+        
+//                gameBoardState[currnetPlayerButtonIndex] = sunder.titleLabel?.text == "X" ? 1 : 2
+                
+                if sunder.titleLabel?.text == "X" {
+                    gameBoardState[currnetPlayerButtonIndex] = 1
+                } else {
+                    gameBoardState[currnetPlayerButtonIndex] = 2
+                }
+
+    }
     private func reset() {
+
+      gameBoardState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        currentPlayer = .one
         for button in gameBoardButtons {
             button.setTitle(" ", for: .normal)
             button.isEnabled = true
+
+            
+            
         }
-        
-        gameBoardState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        currentPlayer = .one
     }
-
 }
-
